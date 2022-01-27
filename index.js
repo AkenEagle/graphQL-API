50c4bca9680bb19201b66b5a6aaa10b5ff4b3897
AkenEagle <akeneagle@gmail.com>
2022-01-27 21:04:32 +0000

    Sample file

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const init = async () => {
+  const { url } = await server.listen();
+
+  console.log(`Server is running on ${url}`);
+};
+
+init();
---
+++ sample.js
@@ -0,0 +1,33 @@
+const london = {
+  name: "London",
+  id: 2643743,
+
+  weather: [
+    {
+      id: 803,
+      main: "Clouds",
+      description: "broken clouds",
+      icon: "04n",
+    },
+  ],
+
+  statistics: {
+    temp: 278.63,
+    feels_like: 277.02,
+    temp_min: 275.96,
+    temp_max: 281.07,
+    pressure: 1033,
+    humidity: 78,
+  },
+
+  visibility: 10000,
+
+  wind: {
+    speed: 2.06,
+    deg: 280,
+  },
+
+  clouds: {
+    all: 67,
+  },
+};
